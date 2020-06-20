e68ac4f4aa682c1fdc8484812bd4e29ef761f07e
Joel Christner <joel.christner@gmail.com>
2020-06-19 23:55:07 -0700

    Mysql fix

--- DatabaseWrapper.Mysql/MysqlHelper.cs
+++ DatabaseWrapper.Mysql/MysqlHelper.cs
@@ -124,8 +124,7 @@ namespace DatabaseWrapper.Mysql
                 ") " +
                 "ENGINE=InnoDB " +
                 "AUTO_INCREMENT=1 " +
-                "DEFAULT CHARSET=utf8mb4 " +
-                "COLLATE=utf8mb4_0900_ai_ci";
+                "DEFAULT CHARSET=utf8mb4 ";
 
             return query;
         }
